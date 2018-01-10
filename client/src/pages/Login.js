77161958f3548bfe4cd57cc87fcbb305e24be350
Daniel Testa <dantesta@Daniels-MacBook-Pro.local>
2018-01-09 19:16:08 -0500

    login update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,31 +5,41 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 
 const Login = () =>
-  <div>
-    <Hero backgroundImage="http://www.jillstable.ca/wp-content/uploads/gift-banner.jpg">
-      <h1>Test</h1>
-      <h2>Gift Baskets & More!</h2>
-    </Hero>
-    <Container style={{ marginTop: 30 }}>
-      <Row>
-        <Col size="md-12">
-          <h1>Welcome To Basket Builder!</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="md-12">
-          <p>
-            Create a new basket
-          </p>
-          <p>
-            Purchase Existing Basket
-          </p>
-          <p>
-            Sign-in
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  </div>;
+  <div className="container">
+  <div className="row">
+    <div className="col-sm-6 col-md-4 col-md-offset-4">
+      <h1 className="text-center login-title">Sign in 
+        <div className="account-wall"><img className="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"/>
+          <form className="form-signin" action="/login" method="POST">
+            <input className="form-control" type="text" name="username" placeholder="Email" required="required" autofocus="autofocus"/>
+            <input className="form-control" type="password" name="password" placeholder="Password" required="required"/>
+            <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button><span className="clearfix"></span>
+          </form>
+        </div><a className="text-center new-account" href="/signup">Create an account</a>
+      </h1>
+    </div>
+  </div>
+
+
+
+  <div className="row">
+    <div className="col-sm-6 col-md-4 col-md-offset-4">
+      <h1 className="text-center login-title">Registration Details
+        <div className="signup-wall">
+          <form className="form-signin" action="/signup" method="POST">
+            <input className="form-control" type="text" name="username" placeholder="Username" required="required" autofocus="autofocus"/>
+            <input className="form-control" type="password" name="password" placeholder="Password" required="required"/>
+            <input className="form-control" type="email" name="email" placeholder="Email" required="required"/>
+            <input className="form-control" type="text" name="firstName" placeholder="First Name" required="required"/>
+            <input className="form-control" type="text" name="lastName" placeholder="Last Name" required="required"/>
+            <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button><span className="clearfix"></span>
+          </form>
+        </div>
+        <div id="message">
+        </div>
+      </h1>
+    </div>
+  </div>
+</div>  
 
 export default Login;
