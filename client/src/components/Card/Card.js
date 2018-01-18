d5a4221e1cb3f2b6c04fa650202439dc6b271e4a
Daniel Testa <dantesta@Daniels-MacBook-Pro.local>
2018-01-17 21:09:56 -0500

    prices added

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,15 +2,23 @@ import React from "react";
 import "./Card.css";
 
 const Card = props =>
+
+<div>
   <div
     className="card"
     style={{
       backgroundImage: props.image ? `url(${props.image})` : "none"
     }}
-    onClick={props.onClick ? (() => props.onClick(props.image)) : (() => console.log('hi'))}
+    onClick={props.onClick ? (() => props.onClick(props.image, props.price)) : (() => console.log('hi'))}
   >
     {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
+
     
-  </div>;
+  </div>
+  <div><p>{props.price}</p>
+  </div>
+
+</div>;
+
 
 export default Card;
